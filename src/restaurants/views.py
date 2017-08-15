14d30df1851ce28eddf44f624af8337e2bf49e5d
hiro <nathanwdavis@gmail.com>
2017-08-14 21:07:04 -0500

    Added a for loop in the template

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -2,19 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
+# def home(request):
     
-    html_ = """
-    <!DOCTYPE html>
-    <html lang = en>
-        <head>
-        </head>
-        <body>
-            <h1>Hello World!</h1>
-            <p>Nathan is rad!</p>
-        </body>
-    </html>
-    """
+#     html_ = """
+#     <!DOCTYPE html>
+#     <html lang = en>
+#         <head>
+#         </head>
+#         <body>
+#             <h1>Hello World!</h1>
+#             <p>Nathan is rad!</p>
+#         </body>
+#     </html>
+#     """
 
-    return HttpResponse(html_)
-    #return render(request, "home.html",{})
+#     return HttpResponse(html_)
+#     #return render(request, "home.html",{})
+def home(request):
+    return render(request, "base.html", {})
