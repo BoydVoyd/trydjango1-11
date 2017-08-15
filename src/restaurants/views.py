9e27a30274d006ea8f03c71610d448b7559e0173
hiro <nathanwdavis@gmail.com>
2017-08-14 21:07:24 -0500

    Added a for loop in template

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
-# def home(request):
-    
-#     html_ = """
-#     <!DOCTYPE html>
-#     <html lang = en>
-#         <head>
-#         </head>
-#         <body>
-#             <h1>Hello World!</h1>
-#             <p>Nathan is rad!</p>
-#         </body>
-#     </html>
-#     """
 
-#     return HttpResponse(html_)
-#     #return render(request, "home.html",{})
 def home(request):
-    return render(request, "base.html", {})
+    num = random.randint(0,10000)
+    some_list = [num, random.randint(0,10000), random.randint(0,10000)]
+    context = {
+        "html_var":"rad", 
+        "num":num, "bool_item":True, 
+        "some_list": some_list
+        }
+    return render(request, "base.html", context)
