041ad69833b1feb3255c7cc93ac4dbc462cc1cb9
hiro <nathanwdavis@gmail.com>
2017-08-14 20:07:32 -0500

    removed fstring. need to upgrade to python 3.6 for that to work.

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     
-    html_var = 'rad!'
     html_ = """
     <!DOCTYPE html>
     <html lang = en>
@@ -12,7 +11,7 @@ def home(request):
         </head>
         <body>
             <h1>Hello World!</h1>
-            <p>Nathan is {html_var}</p>
+            <p>Nathan is rad!</p>
         </body>
     </html>
     """
