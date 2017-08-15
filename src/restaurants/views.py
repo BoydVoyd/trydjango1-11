0722bb417a77532ab824951db233a68caaae5f89
hiro <nathanwdavis@gmail.com>
2017-08-14 19:55:39 -0500

    Added some html

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -3,5 +3,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello")
+    
+    html_ = """
+    <!DOCTYPE html>
+    <html lang = en>
+        <head>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+            <p>Nathan is rad!</p>
+        </body>
+    </html>
+    """
+
+    return HttpResponse(html_)
     #return render(request, "home.html",{})
