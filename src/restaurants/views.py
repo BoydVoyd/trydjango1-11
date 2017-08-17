e67beadfda1905dc3ba7875fb0156458b7b3bde2
hiro <nathanwdavis@gmail.com>
2017-08-17 18:40:01 -0500

    added hom.html home2.html home3.html

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -12,4 +12,20 @@ def home(request):
         "num":num, "bool_item":True, 
         "some_list": some_list
         }
-    return render(request, "base.html", context)
+    return render(request, "home.html", context)
+
+def home2(request):
+    num = random.randint(0,10000)
+    some_list = [num, random.randint(0,10000), random.randint(0,10000)]
+    context = {
+
+        }
+    return render(request, "home2.html", context)
+
+def home3(request):
+    num = random.randint(0,10000)
+    some_list = [num, random.randint(0,10000), random.randint(0,10000)]
+    context = {
+
+        }
+    return render(request, "home3.html", context)
